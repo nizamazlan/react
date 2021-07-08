9101e48001d6fba7b38f3cbee187329223b504c5
Nizam Azlan <nizamazlan99@gmail.com>
2021-07-08 17:41:54 +0800

    Day 1

--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -2,4 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('root'));
+const fname = "Nizam";
+const lname = "Azlan";
+const year = (new Date().getFullYear());
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading" contentEditable="true" spellCheck="false">Hello {`${fname} ${lname}`}</h1>
+    <p>This is a paragraph. Created by {fname}</p>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+    <p>This is a second paragraph. Copyright {year}</p>
+
+    <div>
+      <img src="https://picsum.photos/200/400"/>
+      <img src="https://picsum.photos/200"/>
+      <img src="https://picsum.photos/200"/>
+    </div>
+  </div>,
+document.getElementById('root'));
