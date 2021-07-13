03c07707811a2f401fcf6cc43bf41bd161b6f03f
Nizam Azlan <nizamazlan99@gmail.com>
2021-07-13 15:20:54 +0800

    Day 2

--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -5,6 +5,23 @@ import ReactDOM from 'react-dom';
 const fname = "Nizam";
 const lname = "Azlan";
 const year = (new Date().getFullYear());
+const time = new Date().getHours();
+let greetings;
+
+const customStyle = {
+  color: ""
+}
+
+if (time < 12){
+  greetings = "Good Morning";
+  customStyle.color = "blue";
+} else if (time < 18) {
+  greetings = "Good Evening";
+  customStyle.color = "green";
+} else {
+  greetings = "Good Night";
+  customStyle.color = "yellow";
+}
 
 ReactDOM.render(
   <div>
@@ -18,9 +35,15 @@ ReactDOM.render(
     <p>This is a second paragraph. Copyright {year}</p>
 
     <div>
-      <img src="https://picsum.photos/200/400"/>
-      <img src="https://picsum.photos/200"/>
-      <img src="https://picsum.photos/200"/>
+      <img alt="1" src="https://picsum.photos/200/400"/>
+      <img alt="1" src="https://picsum.photos/200"/>
+      <img alt="1" src="https://picsum.photos/200"/>
     </div>
+
+    <div>
+      <h1 className="heading1" style={customStyle}> {greetings} </h1>
+      <h2>{time}</h2>
+    </div>
+
   </div>,
 document.getElementById('root'));
