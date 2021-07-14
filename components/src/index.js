28bfbb80b4326dd530633fa5d108c27d1201c426
Nizam Azlan <nizamazlan99@gmail.com>
2021-07-14 16:24:03 +0800

    keeper apps

--- components/src/index.js
+++ components/src/index.js
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx"
+import {add, multiply, divide} from "./calc.jsx"
 
 
 ReactDOM.render(
-  <App/>,
+  <div>
+    <ul>
+      <li>{add(1,2)}</li>
+      <li>{multiply(2,3)}</li>
+      <li>{divide(5,2)}</li>
+    </ul>
+  <App/>
+  </div>,
   document.getElementById('root')
 );
